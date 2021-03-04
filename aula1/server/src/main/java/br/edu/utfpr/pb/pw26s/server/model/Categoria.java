3e09b3e11bdd8dc58b9d16886108cd8ad3eec60f
Vinicius Pegorini <viniciuspegorini@gmail.com>
2021-03-04 19:35:37 -0300

    aula1

---
+++ aula1/server/src/main/java/br/edu/utfpr/pb/pw26s/server/model/Categoria.java
@@ -0,0 +1,16 @@
+package br.edu.utfpr.pb.pw26s.server.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@EqualsAndHashCode(of = {"id"})
+@NoArgsConstructor
+@AllArgsConstructor
+public class Categoria {
+}
